37c0dd1a5dd39234467039875a1d9ad3aac3be83
ab6ul3ziz <123942600+ab6ul3ziz@users.noreply.github.com>
2025-01-13 15:56:16 +0200

    upload things

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -11,7 +11,7 @@ import {
 
 const adminRouter = express.Router();
 
-adminRouter.get("/posts", ClerkExpressRequireAuth(), getPendingPosts);
+adminRouter.get("/posts", ClerkExpressWithAuth(), getPendingPosts);
 adminRouter.patch(
   "/posts/reject/:postId",
   ClerkExpressRequireAuth(),
