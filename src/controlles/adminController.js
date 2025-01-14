b985c3532a301e09e979b26c3304386bfcaaaf5b
ab6ul3ziz <123942600+ab6ul3ziz@users.noreply.github.com>
2025-01-14 17:50:43 +0200

    upload things

--- src/controlles/adminController.js
+++ src/controlles/adminController.js
@@ -10,59 +10,61 @@ export const getPendingPosts = async (req, res) => {
       return res.status(401).json(error);
     }
   }
-  return res.status(401).json({ error: req.auth });
+  return res.status(401).json({ error: "Unauthorized!" });
 };
 
 export const rejectPost = async (req, res) => {
-  console.log(req.auth);
+  if (req.auth?.claims?.metadata?.role === "admin") {
+    const { rejectionReasons, userId } = req.body;
+    const notification = {
+      type: "rejected",
+      ...rejectionReasons,
 
-  const { rejectionReasons, userId } = req.body;
-  const notification = {
-    type: "rejected",
-    ...rejectionReasons,
+      seen: false,
+    };
+    try {
+      const sendNotification = await User.updateOne(
+        { clerk_Id: userId },
+        { $push: { notifications: notification } }
+      );
+      await PostModel.findByIdAndUpdate(req.params.postId, {
+        status: "rejected",
+      });
 
-    seen: false,
-  };
-  try {
-    const sendNotification = await User.updateOne(
-      { clerk_Id: userId },
-      { $push: { notifications: notification } }
-    );
-    await PostModel.findByIdAndUpdate(req.params.postId, {
-      status: "rejected",
-    });
-    console.log(sendNotification);
-    res.status(200).json({ status: "true" });
-  } catch (error) {
-    console.log(error);
+      return res.status(200).json({ status: "true" });
+    } catch (error) {
+      console.log(error);
+    }
   }
+  return res.status(401).json({ error: "Unauthorized!" });
 };
 
 export const approvePost = async (req, res) => {
-  console.log(req.auth);
+  if (req.auth?.claims?.metadata?.role === "admin") {
+    const { userId } = req.body;
+    const postId = req.params.postId;
+    const post = await PostModel.findById(postId);
+    const notification = {
+      type: "approved",
+      postId,
+      postName: post.postName,
 
-  const { userId } = req.body;
-  const postId = req.params.postId;
-  const post = await PostModel.findById(postId);
-  const notification = {
-    type: "approved",
-    postId,
-    postName: post.postName,
-
-    seen: false,
-  };
-  try {
-    await PostModel.findByIdAndUpdate(postId, {
-      status: "approved",
-    });
-    await User.updateOne(
-      { clerk_Id: userId },
-      {
-        $push: { notifications: notification },
-      }
-    );
-    res.status(200).json({ status: "true" });
-  } catch (error) {
-    console.log(error);
+      seen: false,
+    };
+    try {
+      await PostModel.findByIdAndUpdate(postId, {
+        status: "approved",
+      });
+      await User.updateOne(
+        { clerk_Id: userId },
+        {
+          $push: { notifications: notification },
+        }
+      );
+      return res.status(200).json({ status: "true" });
+    } catch (error) {
+      console.log(error);
+    }
   }
+  return res.status(401).json({ error: "Unauthorized!" });
 };
